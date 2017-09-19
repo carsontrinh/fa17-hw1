10ca6ed8c2d10b802ad7016e14a4fd31b1bef7d9
Carson Trinh <trinh.carson@berkeley.edu>
2017-09-19 11:04:44 -0700

    All test cases passed

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,7 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    return a.map{|s| s.to_i + 2}.find_all{|e| e % 2 == 0 && e < 10}.uniq.reduce(0, :+)
   end
 end
 
